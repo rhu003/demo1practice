d32b77cc465f299a4bba8a0a8f4e9648e1c0ecc3
rhu003 <rhu@ucsd.edu>
2022-03-18 09:22:56 -0700

    libs + code that fails

---
+++ demoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class demoTest {
+    @Test
+    public void multiplication(){
+        assertEquals(2, demo.multiply(1, 3));
+    }
+}
